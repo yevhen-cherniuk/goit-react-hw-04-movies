4254865cbdec4a2dbf91d7b8bfa85ad3ea7d874c
yevhen-cherniuk <chernuy1@gmail.com>
2021-06-27 22:54:40 +0400

    mvp

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,5 @@
 import React from "react";
-//Components
 import Navigation from "../Navigation/Navigation";
-// import { Layout } from "antd";
-//styles
 import style from '../Header/Header.module.css';
 
 const Header = () => (
